882646507b89d9fc20336d2b92c58869f0c26c24
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-10-15 18:30:15 +0400

    Added two constructors and method print()

--- matrix.cpp
+++ matrix.cpp
@@ -1,12 +1,76 @@
 #include <iostream>
+#include <random> // generate random number
 
 template<class T>
 class Matrix
 {
 private:
-    int _data**;
-    int _row;
-    int _columns;
+    size_t _rows;
+    size_t _cols;
+    T** _data;
 public:
-    Matrix(int row, int columns, int data);
+    Matrix(size_t r, size_t c, T value)
+    {
+        this->_rows = r;
+        this->_cols = c;
+        _data = new T*[_rows];
+        for(size_t i = 0; i < _rows; i++)
+        {
+            _data[i] = new T[_cols];
+            for(size_t j = 0; j < _cols; j++)
+            {
+                _data[i][j] = value;
+            }
+        }
+    }
+    void print()
+    {
+        for (int i = 0; i < _rows; i++) {
+            for (int j = 0; j < _cols; j++) {
+                std::cout << _data[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    
+    Matrix(size_t r, size_t c, T low, T up)
+    {
+        this->_rows = r;
+        this->_cols = c;
+        _data = new T*[_rows];
+        std::mt19937 rng(std::random_device{}());
+        std::uniform_int_distribution<T> distribution(low, up);
+        for(size_t i = 0; i < _rows; i++)
+        {
+            _data[i] = new T[_cols];
+            for(size_t j = 0; j < _cols; j++)
+            {
+                _data[i][j] = distribution(rng);
+            }
+        }
+    }
+
+
+    T& operator()(size_t r, size_t c);
+    Matrix operator+(const Matrix& other);
+    Matrix operator-(const Matrix& other);
+
+    T getRows()
+    {
+        return _rows;
+    }
+
+    T getCols()
+    {
+        return _cols;
+    }
 };
+
+int main()
+{
+    Matrix a(2, 2, 4);
+    a.print();
+    std::cout << std::endl;
+    Matrix b(3, 3, 1, 3);
+    b.print();
+}

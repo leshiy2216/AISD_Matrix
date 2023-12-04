0e31e53bd54f77a181901ee699649811287363f0
leshiy2216 <cyberstanislaw2004@gmail.com>
2023-12-04 19:50:41 +0400

    start work

---
+++ lab1/AISD_Matrix/AISD_Matrix.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <random> // generate random number
+#include <complex>
+#include <stdexcept>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    std::cout << "Hellop" << std::endl;
+
+    return 0;
+}
